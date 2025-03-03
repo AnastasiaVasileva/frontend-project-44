248859cffd64cbf04f9928620847dfd5c03bec94
AVas <vao1404@gmail.com>
2025-03-03 14:37:44 +0300

    try to fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line consistent-return
 const runGraetings = (rules, generateRound) => {
   const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
 
@@ -13,13 +14,24 @@ const runGraetings = (rules, generateRound) => {
     const [question, correctAnswer] = generateRound();
 
     console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
+    let userAnswer = readlineSync.question('Your answer: ');
+    if (correctAnswer === String) {
+      userAnswer = String(userAnswer);
+    }
+    if (correctAnswer === Number) {
+      userAnswer = Number(userAnswer);
+    }
 
     if (correctAnswer === userAnswer) {
       console.log('Correct!');
       correctResult += 1;
     } else {
+      console.log(typeof userAnswer);
+      console.log(typeof correctAnswer);
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}!`);
+      console.log(typeof userAnswer);
+      console.log(typeof correctAnswer);
+      // eslint-disable-next-line consistent-return
       return;
     }
   }
