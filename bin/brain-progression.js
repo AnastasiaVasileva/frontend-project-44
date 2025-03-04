123df98ee5d12f77242e4fa54537449c5e21cc1f
AVas <vao1404@gmail.com>
2025-03-04 21:05:36 +0300

    fix brain-prog

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,31 +1,33 @@
 #!/usr/bin/env node
 import runGraetings from '../src/index.js';
 
-function progGame() {
-  // eslint-disable-next-line no-use-before-define
+const generateRandomProgression = (start, stepNumber, hiddenIndex) => {
+  const progression = [];
+
+  for (let i = 0; i < 10; i += 1) {
+    progression.push(start + i * stepNumber);
+  }
+
+  const number = progression[hiddenIndex];
+  progression[hiddenIndex] = '..';
+
+  return [progression.join(' '), String(number)];
+};
+
+const generateRound = () => {
+  const arrayLength = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+  const maxValue = 100;
+  const stepNumber = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+  const start = Math.floor(Math.random() * (maxValue - (arrayLength - 1) * stepNumber + 1));
+  const hidden = Math.floor(Math.random() * arrayLength);
+
+  const [question, answer] = generateRandomProgression(start, stepNumber, hidden);
+
+  return [question, answer];
+};
+
+function runProgressionGame() {
   runGraetings('What number is missing in the progression?', generateRound);
 }
 
-function generateRound() {
-  const correctArray = [];
-  const arrayLength = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
-  const maxValue = 100;
-  const step = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-  const startValue = Math.floor(Math.random() * (maxValue - (arrayLength - 1) * step + 1));
-
-  for (let i = 0; i < arrayLength; i += 1) {
-    correctArray.push(startValue + i * step);
-  }
-
-  const forUserArray = [...correctArray];
-
-  const randomIndex = Math.floor(Math.random() * arrayLength);
-  forUserArray[randomIndex] = '..';
-  const newArr = forUserArray;
-
-  const question = newArr.join(' ');
-  const answer = correctArray[randomIndex];
-  return [question, String(answer)];
-}
-
-progGame();
+runProgressionGame();
